f8ae59e73deeca9ef645bd98d376777b79859405
meathul <85130069+meathul@users.noreply.github.com>
2025-08-12 09:25:08 +0530

    error handling middleware

---
+++ Week3/error.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+let errorCount = 0;
+
+// You have been given an express server which has a few endpoints.
+// Your task is to
+// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
+// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
+
+app.get('/user', function(req, res) {
+  throw new Error("User not found");
+  res.status(200).json({ name: 'john' });
+});
+
+app.post('/user', function(req, res) {
+  res.status(200).json({ msg: 'created dummy user' });
+});
+
+app.get('/errorCount', function(req, res) {
+  res.status(200).json({ errorCount });
+});
+
+// Global error handler: increment errorCount and return 404 on any exception
+app.use((err, req, res, next) => {
+    errorCount++;
+    res.status(404).json({ msg: "Error occured", error: err.message });
+});
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
