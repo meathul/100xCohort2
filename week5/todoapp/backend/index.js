3122a46cf8fa04e0c3c67336136d8d549546de2b
meathul <85130069+meathul@users.noreply.github.com>
2025-08-23 00:34:22 +0530

    post-routes

--- week5/todoapp/backend/index.js
+++ week5/todoapp/backend/index.js
@@ -1,23 +1,34 @@
+const { createTodo } = require("./types");
+const{updateTodo} = require("./types")
 const express= require(express);
 const bodyparser= require(bodyparser);
 const app = express();
 const PORT=3000;
 app.use(express.json());
 app.post("/todo",function(req,res){
-//middleware
-//input validation
-//todo logic
+    const createPayload=req.body;
+    const parsedPayload=createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"Input is Invalid",
+        })
+        return
+    }
+    //put in mongodb
 })
 app.get("/todos",function(req,res){
-//middlware
-//input validation
-//get todos
+
 })
 
 app.put("/completed",function(req,res){
-//middleware
-//input validation
-//todo logic
+    const updatePayload=req.body;
+    const parsedPayload=updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"Input is Invalid",
+        })
+        return
+    }
 })
 app.listen(PORT,()=>{
     console.log("SERVER RUNNING");
