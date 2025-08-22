cd39646edca9a88350593078990866ec6b494330
meathul <85130069+meathul@users.noreply.github.com>
2025-08-23 01:14:23 +0530

    db

--- week5/todoapp/backend/db.js
+++ week5/todoapp/backend/db.js
@@ -1,7 +1,12 @@
- require('dotenv').config();
+require('dotenv').config();
 const mongoose= require(mongoose);
+mongoose.connect(connectionstring)
 const todoSchema=mongoose.schema({
     title: String,
     description:String,
     completed : Boolean
 })
+const todo= mongoose.model("todos", todoSchema);
+module.export={
+    todo: todo
+}
--- week5/todoapp/backend/index.js
+++ week5/todoapp/backend/index.js
@@ -1,11 +1,12 @@
 const { createTodo } = require("./types");
-const{updateTodo} = require("./types")
+const{updateTodo} = require("./types");
+const { todo }= require("./db");
 const express= require(express);
 const bodyparser= require(bodyparser);
 const app = express();
 const PORT=3000;
 app.use(express.json());
-app.post("/todo",function(req,res){
+app.post("/todo",async function(req,res){
     const createPayload=req.body;
     const parsedPayload=createTodo.safeParse(createPayload);
     if(!parsedPayload.success){
@@ -14,13 +15,24 @@ app.post("/todo",function(req,res){
         })
         return
     }
-    //put in mongodb
+    await todo.create({
+        title: createPayload.title,
+        description : createPayload.description,
+        completed: false
+    })
+    res.json({
+        msg:"Todo Created"
+    })
 })
-app.get("/todos",function(req,res){
+app.get("/todos",async function(req,res){
+    const todos= await todo.find({});
+    res.json({
+        todos
+    })
 
 })
 
-app.put("/completed",function(req,res){
+app.put("/completed",async function(req,res){
     const updatePayload=req.body;
     const parsedPayload=updateTodo.safeParse(updatePayload);
     if(!parsedPayload.success){
@@ -29,6 +41,14 @@ app.put("/completed",function(req,res){
         })
         return
     }
+    await todo.update({
+        _id:req.body.id
+    },{
+        completed:true
+    })
+    res.json({
+        msg:"todo marked as completed"
+    })
 })
 app.listen(PORT,()=>{
     console.log("SERVER RUNNING");
